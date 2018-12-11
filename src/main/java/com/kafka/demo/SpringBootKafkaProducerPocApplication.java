35cfcbcb163bdd90e58c50cd55a2f31f60d3f35c
412619 <412619@PC347653.cts.com>
2018-12-11 16:01:53 +0530

    initial commit

---
+++ src/main/java/com/kafka/demo/SpringBootKafkaProducerPocApplication.java
@@ -0,0 +1,12 @@
+package com.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProducerPocApplication.class, args);
+	}
+}
